5e08e85971e220f98e84868dc131eb5ebd4a7ec3
paperplane110 <1374736640@qq.com>
2024-11-16 23:14:49 +0800

    Polish(todo done): show task table for done task

--- todo_cmd/sub_cmd/todo_done.py
+++ todo_cmd/sub_cmd/todo_done.py
@@ -4,6 +4,7 @@ from typing import Tuple
 import rich_click as click
 
 import todo_cmd.templates as t
+from todo_cmd import show
 from todo_cmd.language import TRANS
 from todo_cmd.interface.todo import todo_interface
 
@@ -21,6 +22,7 @@ def done(ids:Tuple[int]):
     invalid_ids = []
     valid_ids = []
     already_done_ids = []
+    mod_success_task_list = []
     for id in ids:
         task = todo_interface.find_by_id(id)
         if task:
@@ -29,6 +31,7 @@ def done(ids:Tuple[int]):
             else:
                 task.update_status("done")
                 valid_ids.append(id)
+                mod_success_task_list.append(task)
         else:
             invalid_ids.append(id)
     todo_interface.save_todos()
@@ -40,6 +43,7 @@ def done(ids:Tuple[int]):
         t.info(f"{TRANS('task_already_done')}: {already_done_ids}")
 
     if len(valid_ids):
+        show.table(mod_success_task_list)
         t.done(f"{TRANS('task_done')}: {valid_ids}")
 
 
